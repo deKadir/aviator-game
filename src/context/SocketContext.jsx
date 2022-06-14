dec17da948b9f31099dc08b0e4689186678ef38e
kadir <develioglu306@gmail.com>
2022-06-14 21:45:33 +0300

    Change base url

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -3,7 +3,7 @@ import socketIOClient from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = socketIOClient('https://git.heroku.com/sleepy-bayou-78676.git');
+const socket = socketIOClient('https://sleepy-bayou-78676.herokuapp.com/');
 const SocketContextProvider = (props) => {
   const [countdown, setCountdown] = useState(0);
   useEffect(() => {
