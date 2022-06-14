060c67b034ae0507b675f7bcb3d22b2d3b15b886
kadir <develioglu306@gmail.com>
2022-06-14 22:51:06 +0300

    change base url

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,15 +1,17 @@
 import Card from 'components/global/card';
 import { SocketContext } from 'context/SocketContext';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import style from './chat.module.scss';
 export default function Chat() {
   const [messages, setMessages] = useState([]);
   const { socket } = useContext(SocketContext);
   const [message, setMessage] = useState('');
+  const ref = useRef(null);
   useEffect(() => {
     socket.on('message', (msg) => {
       setMessages([...messages, msg]);
     });
+    ref?.current.scrollIntoView();
   }, [messages]);
   useEffect(() => {
     const submitMessage = (k) => {
@@ -22,25 +24,28 @@ export default function Chat() {
     return () => window.removeEventListener('keypress', submitMessage);
   }, [message]);
   return (
-    <Card className={style.container}>
-      <h2 className={style.title}>Chat</h2>
-      <div className={style.messages}>
-        {messages.map((msg, k) => {
-          return (
-            <div key={k} className={style.message}>
-              <p>{msg.nickname}:</p>
-              <span>{msg.message}</span>
-            </div>
-          );
-        })}
-      </div>
-      <input
-        type="text"
-        className={style.messageInput}
-        placeholder="Start Typing"
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-      />
-    </Card>
+    <>
+      <Card className={style.container}>
+        <h2 className={style.title}>Chat</h2>
+        <div className={style.messages}>
+          {messages.map((msg, k) => {
+            return (
+              <div key={k} className={style.message}>
+                <p>{msg.nickname}:</p>
+                <span>{msg.message}</span>
+              </div>
+            );
+          })}
+          <div ref={ref}></div>
+        </div>
+        <input
+          type="text"
+          className={style.messageInput}
+          placeholder="Start Typing"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+      </Card>
+    </>
   );
 }
--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -3,7 +3,7 @@ import socketIOClient from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = socketIOClient('http://localhost:8080/');
+const socket = socketIOClient('https://sleepy-bayou-78676.herokuapp.com/');
 const SocketContextProvider = (props) => {
   const [countdown, setCountdown] = useState(0);
   const [player, setPlayer] = useState({ nickname: '', balance: 0 });
